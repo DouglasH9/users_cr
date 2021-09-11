a826f739dea8062ef744800b464cd07c28b2546c
Doug Herman <douglash9882@gmail.com>
2021-09-10 23:24:20 -0500

    UsersCR assignment

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
+app.secret_key = 'whale hello there!'
+
+# importing the User class method
+from users import User
+
+@app.route('/')
+def show_form():
+    return render_template('create.html')
+
+@app.route('/read_all')
+def show_all():
+    users = User.get_all()
+    print(users)
+    return render_template('read_all.html', all_users = users)
+
+@app.route('/new_user', methods=["POST"])
+def new_user():
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    User.add_user(data)
+    return redirect('/read_all')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
